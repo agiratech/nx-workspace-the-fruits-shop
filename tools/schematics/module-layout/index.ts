517b4430c3784c3ca54edd3c1eab3c4512f50227
Manikandan Thangaraj <mani@agiratech.com>
2019-08-20 14:57:51 +0530

    Updated custom schematic rules

--- tools/schematics/module-layout/index.ts
+++ tools/schematics/module-layout/index.ts
@@ -1,9 +1,40 @@
 import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
-
+import * as path from 'path';
+ 
 export default function(schema: any): Rule {
-  return chain([
-    externalSchematic('@nrwl/workspace', 'lib', {
-      name: schema.name
-    })
-  ]);
+ if (!schema.name.startsWith('fruits-')) {
+   throw new Error(`All the modules must be prefixed with 'fruits-'`);
+ }
+ 
+ return chain([
+   externalSchematic('@schematics/angular', 'module', {
+     project: schema.project,
+     name: schema.name,
+     routing: true,
+     module: 'app.module.ts'
+   }),
+   externalSchematic('@schematics/angular', 'component', {
+     project: schema.project,
+     name: schema.name,
+     path: path.join(
+       'apps',
+       schema.project,
+       'src',
+       'app',
+       schema.name
+     )
+   }),
+   externalSchematic('@schematics/angular', 'service', {
+     project: schema.project,
+     name: schema.name,
+     path: path.join(
+       'apps',
+       schema.project,
+       'src',
+       'app',
+       schema.name,
+       'services'
+     )
+   })
+ ]);
 }
