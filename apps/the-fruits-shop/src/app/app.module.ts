54ee6d185f2faddcb18213cd64217100106000c4
Manikandan Thangaraj <mani@agiratech.com>
2019-08-20 15:07:45 +0530

    Generated fruit management module using custom schematic

--- apps/the-fruits-shop/src/app/app.module.ts
+++ apps/the-fruits-shop/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FruitsManagementModule } from './fruits-management/fruits-management.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FruitsManagementModule],
   providers: [],
   bootstrap: [AppComponent]
 })
